94bb8bd44bd8ec784ca5c8c97f8c4973ecaed742
Korab Hoxha <korab-h@hotmail.com>
2016-12-17 13:25:50 +0100

    Added describe block on tests

--- spec/routes/v1/prayer_times_spec.rb
+++ spec/routes/v1/prayer_times_spec.rb
@@ -7,42 +7,44 @@ describe Routes::V1::PrayerTimes do
     Routes::V1::API
   end
 
-  it "returns all prayer times for a specific date" do
-    get "", timestamp: Date.today.to_time.to_i
+  describe 'GET /api/v1/prayer_times' do
+    it "returns all prayer times for a specific date" do
+      get "", timestamp: Date.today.to_time.to_i
 
-    expect(last_response.status).to eq(200)
-    expect(last_response.body).to eq({
-      data: [
-        {
-          id: 1,
-          type: "prayer_times",
-          for: "daily",
-          attributes: {
-            date_for: "2016-12-17",
-            fajr: "5:56",
-            shurooq: "7:53",
-            dhuhr: "11:56",
-            asr: "1:35",
-            maghrib: "3:59",
-            isha: "5:49"
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to eq({
+        data: [
+          {
+            id: 1,
+            type: "prayer_times",
+            for: "daily",
+            attributes: {
+              date_for: "2016-12-17",
+              fajr: "5:56",
+              shurooq: "7:53",
+              dhuhr: "11:56",
+              asr: "1:35",
+              maghrib: "3:59",
+              isha: "5:49"
+            }
           }
-        }
-      ],
-      prayer_method_name: "",
-      daylight: 0,
-      timezone: "",
-      latitude: "",
-      longitude: "",
-      city: "",
-      country: "",
-      qibla_direction: ""
-    }.to_json)
-  end
+        ],
+        prayer_method_name: "",
+        daylight: 0,
+        timezone: "",
+        latitude: "",
+        longitude: "",
+        city: "",
+        country: "",
+        qibla_direction: ""
+      }.to_json)
+    end
 
-  it "returns prayer times for a specific month" do 
-  end
+    it "returns prayer times for a specific month" do 
+    end
 
-  it "UNIX timestamp for the specified time zone" do 
+    it "UNIX timestamp for the specified time zone" do 
+    end
   end
 end
 
