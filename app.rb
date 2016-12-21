15b71649942a7ec5115c835aa15663d402631a34
Korab Hoxha <korab-h@hotmail.com>
2016-12-21 17:40:35 +0100

    Added script/stylesheet tags helpers

--- app.rb
+++ app.rb
@@ -1,19 +1,32 @@
 require "date"
 require "sinatra"
-
+require "CGI"
 class App < Sinatra::Base
   set :root, File.dirname(__FILE__)
 
   helpers do
+    def javascript_include_tags(*sources)
+      html = ""
+      sources.each do |path|
+        html << "<script src=\"#{path}\"></script>"
+      end
+      html
+    end
+
+    def stylesheet_include_tags(*sources)
+      html = ""
+      sources.each do |path|
+        html << "<link rel='stylesheet' href=\"#{path}\">"
+      end
+      html
+    end
   end
 
   get "/" do
     date = Date.today.strftime("%d-%m-%Y")
     timings = Timings.find_by_date(date)
 
-    erb :index, locals: {
-      timings: timings
-    }
+    erb :index, locals: { timings: timings }
   end
 end
 
