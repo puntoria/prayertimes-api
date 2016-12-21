e51d0e7e4e8de6b1f16e719a306867a38c900611
Korab Hoxha <korab-h@hotmail.com>
2016-12-21 05:35:56 +0100

    Default /root_path view

--- app.rb
+++ app.rb
@@ -2,14 +2,18 @@ require "date"
 require "sinatra"
 
 class App < Sinatra::Base
-  helpers do 
+  set :root, File.dirname(__FILE__)
+
+  helpers do
   end
 
   get "/" do
     date = Date.today.strftime("%d-%m-%Y")
     timings = Timings.find_by_date(date)
 
-    erb :index, locals: { timings: timings }
+    erb :index, locals: {
+      timings: timings
+    }
   end
 end
 
--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,6 @@ $:.unshift Config.root.join('lib')
 OTR::ActiveRecord.configure_from_file! Config.root.join('config', 'database.yml')
 
 [
-  %w(app web ** *.rb),
   %w(app models ** *.rb),
   %w(app entities ** *.rb),
   %w(app routes v* *.rb),
